60a9038e4ed5a44a682d871507fd0b8518955f07
Federico Pellegatta <pellegatta.federico@gmail.com>
2023-10-27 15:54:42 +0200

    Add report header and Thymeleaf template processing
    The existing PdfGeneratorService has been enhanced to support report header information and processing HTML templates.

--- src/main/java/dev/federicopellegatta/samplepdfreport/service/PdfGeneratorService.java
+++ src/main/java/dev/federicopellegatta/samplepdfreport/service/PdfGeneratorService.java
@@ -1,15 +1,49 @@
 package dev.federicopellegatta.samplepdfreport.service;
 
+import dev.federicopellegatta.samplepdfreport.dto.StudentResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.dialect.SpringStandardDialect;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Collection;
 
 @Service
 @AllArgsConstructor
 @Slf4j
 public class PdfGeneratorService {
+	private final StudentService studentService;
+	
 	public String generatePdfReport() {
-		// TODO
+		Collection<StudentResponse> students = studentService.allStudents();
+		String html = parseHtmlTemplate(students);
+		
 		return "";
+	}
+	
+	private String parseHtmlTemplate(Collection<StudentResponse> students) {
+		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+		templateResolver.setPrefix("templates/"); // thymeleaf searches for templates in resources/templates
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		
+		TemplateEngine templateEngine = new TemplateEngine();
+		templateEngine.setDialect(new SpringStandardDialect());
+		templateEngine.setTemplateResolver(templateResolver);
+		
+		Context context = new Context();
+		context.setVariable("class", "4D");
+		context.setVariable("header", new ReportHeader("School register", "A students report"));
+		context.setVariable("students", students);
+		
+		return templateEngine.process("school_register", context);
 	}
 }
---
+++ src/main/java/dev/federicopellegatta/samplepdfreport/service/ReportHeader.java
@@ -0,0 +1,11 @@
+package dev.federicopellegatta.samplepdfreport.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ReportHeader {
+	private String title;
+	private String description;
+}
