1d7091f32a36a2ab227b882fb6466424e8a2130c
Federico Pellegatta <pellegatta.federico@gmail.com>
2023-11-17 11:07:00 +0100

    Simplify PdfPageStudentMapper and remove parallel configuration
    In this commit unnecessary complexity in the PdfPageStudentMapper class was removed. Specifically configuration property `pdf.mapper.min-parallel-pages` enabling dynamic selection between parallel and non-parallel processing based on the number of pages was removed from application.yml and corresponding logic from PdfPageStudentMapper class. Now, PdfPageStudentMapper always processes in parallel. Actually, even with a restricted number of pages, parallel processing has been proven to be the most efficient option.

--- src/main/java/dev/federicopellegatta/samplepdfreport/service/PdfPageStudentMapper.java
+++ src/main/java/dev/federicopellegatta/samplepdfreport/service/PdfPageStudentMapper.java
@@ -2,40 +2,26 @@ package dev.federicopellegatta.samplepdfreport.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.text.PDFTextStripper;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Slf4j
 public class PdfPageStudentMapper {
 	private final PDDocument document;
-	private final int minParallelPages;
 	
 	public PdfPageStudentMapper(PDDocument document) {
 		this.document = document;
-		ClassLoader loader = Thread.currentThread().getContextClassLoader();
-		Properties properties = new Properties();
-		try {
-			properties.load(loader.getResourceAsStream("application.yml"));
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-		this.minParallelPages = Integer.parseInt(properties.getProperty("pdf.mapper.min-parallel-pages", "10"));
 	}
 	
 	public Map<Integer, String> getPageNumberToStudentNameMap() {
-		return document.getNumberOfPages() <= minParallelPages ? createMapNonParallel() : createMapParallel();
-	}
-	
-	private Map<Integer, String> createMapParallel() {
 		long startTime = System.nanoTime();
-		HashMap<Integer, String> result = IntStream.range(1, document.getNumberOfPages())
+		Map<Integer, String> result = IntStream.range(1, document.getNumberOfPages())
 				.parallel()
 				.mapToObj(pageNumber -> {
 					try {
@@ -46,34 +32,13 @@ public class PdfPageStudentMapper {
 				})
 				.collect(Collectors.toMap(Map.Entry::getKey,
 				                          Map.Entry::getValue,
-				                          (a, b) -> {
-					                          throw new IllegalStateException("Duplicate page number " + a);
+				                          (i, j) -> {
+					                          throw new IllegalStateException("Duplicate page number " + i);
 				                          },
 				                          HashMap::new));
 		long endTime = System.nanoTime();
-		log.info("Tempo di esecuzione in parallelo: {} ms", (endTime - startTime) / 1000000);
+		log.info("Time taken to map page number to student name: {} ms", (endTime - startTime) / 1000000);
 		return result;
-	}
-	
-	private Map<Integer, String> createMapNonParallel() {
-		long startTime = System.nanoTime();
-		
-		int pageNumber = 1;
-		Map<Integer, String> pageNumberToStudentNameMap = new HashMap<>();
-		for (PDPage ignored : document.getPages()) {
-			String studentName;
-			try {
-				studentName = getStudentNameAtPageNumber(pageNumber);
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-			pageNumberToStudentNameMap.put(pageNumber, studentName);
-			++pageNumber;
-		}
-		
-		long endTime = System.nanoTime();
-		log.info("Tempo di esecuzione non in parallelo: {} ms", (endTime - startTime) / 1000000);
-		return pageNumberToStudentNameMap;
 	}
 	
 	private String getStudentNameAtPageNumber(int pageNumber) throws IOException {
